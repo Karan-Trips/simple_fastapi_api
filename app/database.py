dc5ca449391884c5163dc87589d4e3b0bddece35
Karan Tripathi <karant@hyperlinkinfosystem.net.in>
2025-07-08 17:57:12 +0530

    hosting fixed the url

--- app/database.py
+++ app/database.py
@@ -2,7 +2,9 @@ from sqlmodel import  SQLModel, Session, create_engine
 
 # DATABASE_URL = "mysql+pymysql://root@localhost:3306/fastapi_demo" //FOR MYSQL DATABASE
 
-DATABASE_URL = "postgresql+psycopg2://postgres:123@localhost:5432/fastapi"
+# DATABASE_URL = "postgresql+psycopg2://postgres:123@localhost:5432/fastapi" //Local database 
+DATABASE_URL = "postgresql+psycopg2://karan:CMHqmcidIiRvru418E19bd3h89yaE6tG@dpg-d1mgrhidbo4c73fa368g-a.oregon-postgres.render.com/fastapi_h657"
+
 
 engine = create_engine(DATABASE_URL, echo=False,)
 
